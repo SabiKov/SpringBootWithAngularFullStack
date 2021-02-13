23cc90b028e777704d5f310131d3284e0fa76b8e
SabiKov <skovacs33@gmail.com>
2021-02-13 22:52:45 +0100

    added crud op

--- membership/src/main/java/com/sabi11/membership/repo/CustomerRepo.java
+++ membership/src/main/java/com/sabi11/membership/repo/CustomerRepo.java
@@ -3,5 +3,10 @@ package com.sabi11.membership.repo;
 import com.sabi11.membership.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
+    void deleteCustomerById(Long id);
+
+    Optional findCustomerById(Long id);
 }
---
+++ membership/src/main/java/com/sabi11/membership/service/CustomerService.java
@@ -0,0 +1,44 @@
+package com.sabi11.membership.service;
+
+import com.sabi11.membership.model.Customer;
+import com.sabi11.membership.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepo customerRepo;
+
+
+    @Autowired
+    public CustomerService(CustomerRepo customerRepo)  {
+        this.customerRepo = customerRepo;
+    }
+
+    public Customer addCustomer(Customer customer) {
+        customer.setMembershipCode(UUID.randomUUID().toString());
+        return  customerRepo.save(customer);
+    }
+
+    public List<Customer> findAllCustomer() {
+        return customerRepo.findAll();
+    }
+
+    public Customer updateCustomer(Customer customer) {
+        return customerRepo.save(customer);
+    }
+
+    public Customer findCustomerById(Long id) {
+        return customerRepo.findCustomerById(id)
+                .orElseThrow(() -> UserNotFoundexception("User by id " + id + " was not found"));
+    }
+
+    public void  deleteCustomerById(Long id) {
+        customerRepo.deleteCustomerById(id);
+    }
+}
