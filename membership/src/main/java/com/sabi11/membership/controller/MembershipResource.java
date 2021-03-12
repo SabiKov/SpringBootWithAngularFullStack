3a91e957ccd52341cd3968ff072c4dfa279b1cea
SabiKov <skovacs33@gmail.com>
2021-03-12 22:52:18 +0100

    added crud

--- membership/src/main/java/com/sabi11/membership/controller/MembershipResource.java
+++ membership/src/main/java/com/sabi11/membership/controller/MembershipResource.java
@@ -1,4 +1,51 @@
 package com.sabi11.membership.controller;
 
+import com.sabi11.membership.model.Customer;
+import com.sabi11.membership.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/membership")
 public class MembershipResource {
+
+    private final CustomerService customerService;
+
+    public MembershipResource(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customers = customerService.findAllCustomer();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Customer> getCustomerById(
+            @PathVariable("id") Long id) {
+        Customer customer = customerService.findCustomerById(id);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
+        Customer newCustomer = customerService.addCustomer(customer);
+        return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer) {
+        Customer updateCustomer = customerService.updateCustomer(customer);
+        return new ResponseEntity<>(updateCustomer, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteCustomer( @PathVariable("id") Long id) {
+        customerService.deleteCustomerById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
